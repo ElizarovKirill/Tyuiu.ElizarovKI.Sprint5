ad2db7bea2261ad4b8e542b3c07e5b60b33bdf38
ElizarovKirill <144137165+ElizarovKirill@users.noreply.github.com>
2023-11-17 00:57:44 +0500

    Task2

---
+++ Tyuiu.ElizarovKI.Sprint5.Task2.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElizarovKI.Sprint5.Task2.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ElizarovKI.Sprint5.Task2.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("849c1175-0385-4265-a59a-8915e5964985")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.ElizarovKI.Sprint5.Task2.V2/Program.cs
+++ Tyuiu.ElizarovKI.Sprint5.Task2.V2/Program.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.IO;
+using Tyuiu.ElizarovKI.Sprint5.Task2.V2.Lib;
 
-using Tyuiu.ElizarovKI.Sprint5.Task1.V27.Lib;
-
-namespace Tyuiu.ElizarovKI.Sprint5.Task1.V27
+namespace Tyuiu.ElizarovKI.Sprint5.Task2.V2
 {
     class Program
     {
@@ -20,28 +18,39 @@ namespace Tyuiu.ElizarovKI.Sprint5.Task1.V27
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файлy                       *");
-            Console.WriteLine("* Задание #1                                                              *");
-            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
             Console.WriteLine("* Выполнил: Елизаров Кирилл Игоревич | ИИПб-23-2                          *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дана функция                                                            *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов,                    *");
+            Console.WriteLine("* заполненный значениями с клавиатуры.                                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int startValue = -5;
-            int stopValue = 5;
+            int[,] mtrx = { {-7, 7, 5 }, {4, 2, -7 }, {2, 6, -4 } };
 
-            Console.WriteLine("startValue = " + startValue);
-            Console.WriteLine("stopValue = " + stopValue);
+            int rows = mtrx.GetLength(1);
+            int columns = mtrx.GetLength(0);
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+
+                Console.WriteLine();
+            }
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            string res = ds.SaveToFileTextData(startValue, stopValue);
+            string res = ds.SaveToFileTextData(mtrx);
 
             Console.WriteLine($"Файл:  {res}");
             Console.WriteLine("Создан!");
